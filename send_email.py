e909e73c6367608d716510ae754af2efb56bd854
Adit <vyas.swati28@gmail.com>
2025-02-25 13:46:44 +0530

    Changed the Name

--- send_email.py
+++ send_email.py
@@ -6,7 +6,7 @@ def send_email( message):
     my_host = "smtp.gmail.com"
     my_port = 465
     username = "vyasadit879@gmail.com"
-    password = st.secrets['Google']['PASSWORD']
+    password = st.secrets['Google']['Password']
     receiver = "vyasadit879@gmail.com"
     my_context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host=my_host, port=my_port, context=my_context) as server:
